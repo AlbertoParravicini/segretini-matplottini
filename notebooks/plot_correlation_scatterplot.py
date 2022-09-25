8793b2823b8065649bbe4fcddca6caba47abbe9f
Alberto Parravicini <alp@bendingspoons.com>
2022-09-25 15:48:36 +0200

    chore: fix missing palette in scatterplot

--- notebooks/plot_correlation_scatterplot.py
+++ notebooks/plot_correlation_scatterplot.py
@@ -3,9 +3,10 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from segretini_matplottini.utils.correlation_scatterplot import \
+from segretini_matplottini.plot.correlation_scatterplot import \
     correlation_scatterplot
 from segretini_matplottini.utils.plot_utils import save_plot
+from segretini_matplottini.utils.colors import PALETTE_G
 
 ##############################
 # Setup ######################
@@ -14,6 +15,9 @@ from segretini_matplottini.utils.plot_utils import save_plot
 # Axes limits used in the plot, change them accordingy to your data;
 X_LIMITS = (-0.2, 0.6)
 Y_LIMITS = (-0.1, 0.3)
+
+# Color palette used for plotting;
+PALETTE = [PALETTE_G[0], PALETTE_G[2]]
 
 PLOT_DIR = (Path(__file__).parent.parent / "plots").resolve()
 DATA_DIR = (Path(__file__).parent.parent / "data").resolve()
@@ -38,6 +42,7 @@ def plot(data: pd.DataFrame) -> tuple[plt.Figure, plt.Axes]:
         hue="significant",
         xlimits=X_LIMITS,
         ylimits=Y_LIMITS,
+        palette=PALETTE,
         xlabel="Speedup estimate, method A (%)",
         ylabel="Speedup estimate, method B (%)",
     )
